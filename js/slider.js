a96f83711871d5040d371b15672be877d2545f3a
chistiakovnikita <nikita_chnv@mail.ru>
2023-10-30 23:24:13 +0300

    адаптив

--- js/slider.js
+++ js/slider.js
@@ -7,3 +7,22 @@ const servicesSlider = new Swiper('.services__swiper', {
     slidesPerView: 1,
     spaceBetween: 10,
 })
+
+const guarantysSlider = new Swiper('.guarantys__swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true,
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+})
+
+const suppliersSlider = new Swiper('.suppliers__swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true,
+    },
+    slidesPerView: 4,
+})
